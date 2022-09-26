33e6942ba8d8a3c3a7016060c789d0b1670b9a15
Vikrant1983 <114429478+Vikrant1983@users.noreply.github.com>
2022-09-26 22:52:00 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+// function marks(maths, english, GS){
+//     if(maths+english+GS>150){
+//     console.log("your cds marks are:"+"maths:"+maths+"english:"+english+"GS:"+GS+"CONGARTS! QUALIFIED")}
+//     else
+//     {console.log("your cds marks are:"+"maths:"+maths+"english:"+english+"GS:"+GS+"SORRY!NOT QUALIFIED")}
+
+// }
+// marks(50,60,41);
+let name1=prompt("whom do you love??");
+
+function namaste(name){
+    return("\""+name+"\" also loves you soo much.");
+}
+let namaste1= namaste(name1);
+document.write(namaste1);
